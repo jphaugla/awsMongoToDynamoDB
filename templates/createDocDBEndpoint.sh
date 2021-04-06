94d92c354ccbf864dd214111e3d53db09a66cad3
Haugland <jphaugla@3c22fba09f76.ant.amazon.com>
2021-04-06 11:42:31 -0500

    fix docdb endpoint

--- templates/createDocDBEndpoint.sh
+++ templates/createDocDBEndpoint.sh
@@ -4,8 +4,9 @@ aws dms create-endpoint \
            --engine-name docdb \
            --username dbmaster \
            --password dbmaster123 \
-           --server-name dbcluster-juqniyxtumbk.cluster-cykwyngishlk.us-east-1.docdb.amazonaws.com \
+           --server-name dbcluster-6srjbveyndna.cluster-cykwyngishlk.us-east-1.docdb.amazonaws.com \
            --port 27017 \
            --database-name flights \
            --ssl-mode verify-full \
-           --certificate-arn <certificate arn>
+           --doc-db-settings file://docdb-settings.json \
+           --certificate-arn arn:aws:dms:us-east-1:xxxxxxxxx:cert:IYD7MWMJYWXVUFMG4EKVDYYWSXQD2PVBBUVB7EI
--- templates/startReplication.sh
+++ templates/startReplication.sh
@@ -1,1 +1,1 @@
-aws dms start-replication-task --replication-task-arn <replaication task ARN> --start-replication-task-type reload-target
+aws dms start-replication-task --replication-task-arn arn:aws:dms:us-east-1:xxxxxxxxxx:task:L5MORIT5NMBQZRKTMA67G4YJPD7WFSSPUA7G44I --start-replication-task-type reload-target
